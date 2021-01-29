f3d6ef2bfde2da6e99d30a8dcf0781675f5249c2
Bruno André Scatena <bscatena@bestprojects.com.br>
2021-01-29 13:27:36 -0300

    Excel Files in C#

---
+++ ExcelDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata;
+using OfficeOpenXml;
+
+namespace ExcelDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var file = new FileInfo(@"C:\Demos\YouTubeDemo.xlsx");
+
+        }
+
+        static List<PersonModel> GetSetupData()
+        {
+            List<PersonModel> output = new()
+            {
+                new() { Id = 1, FirstName = "Tim", LasttName = "Corey"},
+                new() { Id = 1, FirstName = "Sue", LasttName = "Storm" },
+                new() { Id = 1, FirstName = "Jane", LasttName = "Smith" },
+            }
+        }
+    }
+
+   
+}
