4f12ee04c1d6065b1f9dcb63e86c3b5f59163c2a
Bruno André Scatena <bscatena@bestprojects.com.br>
2021-01-29 13:33:18 -0300

    Criando SaveExcelFile e DeleteIfExists

--- ExcelDemo/Program.cs
+++ ExcelDemo/Program.cs
@@ -2,28 +2,48 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection.Metadata;
+using System.Threading.Tasks;
 using OfficeOpenXml;
 
 namespace ExcelDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
             var file = new FileInfo(@"C:\Demos\YouTubeDemo.xlsx");
 
+            var people = GetSetupData();
+
+            await SaveExcelFile(people, file);
+
         }
 
-        static List<PersonModel> GetSetupData()
+        private static async Task SaveExcelFile(List<PersonModel> people, FileInfo file)
+        {
+            DeleteIfExists(file);
+        }
+
+        private static void DeleteIfExists(FileInfo file)
+        {
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+        }
+
+        private static List<PersonModel> GetSetupData()
         {
             List<PersonModel> output = new()
             {
-                new() { Id = 1, FirstName = "Tim", LasttName = "Corey"},
-                new() { Id = 1, FirstName = "Sue", LasttName = "Storm" },
-                new() { Id = 1, FirstName = "Jane", LasttName = "Smith" },
-            }
+                new() {Id = 1, FirstName = "Tim", LasttName = "Corey"},
+                new() {Id = 1, FirstName = "Sue", LasttName = "Storm"},
+                new() {Id = 1, FirstName = "Jane", LasttName = "Smith"}
+            };
+
+            return output;
         }
     }
 
